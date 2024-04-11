72bf6cb622995e2c6fd568f626a390dcc7213848
SHASHI-17 <hibye31583@gmail.com>
2024-04-12 02:12:04 +0530

    half setup is d9

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -1,53 +1,110 @@
-'use client'
+"use client";
 
+import { useToast } from "@/components/ui/use-toast"
 import { useRouter } from "next/navigation";
-import HomeCard from "./HomeCard"
+import HomeCard from "./HomeCard";
 import { useState } from "react";
 import MeetingModal from "./MeetingModal";
+import { useUser } from "@clerk/nextjs";
+import { Call, useStreamVideoClient } from "@stream-io/video-react-sdk";
 
 const MeetingTypeList = () => {
-    const router = useRouter();
-    const [meetingState, setMeetingState] = useState<
-    'isScheduleMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined
+  const router = useRouter();
+  const [meetingState, setMeetingState] = useState<
+    "isScheduleMeeting" | "isJoiningMeeting" | "isInstantMeeting" | undefined
   >(undefined);
 
- const createMeeting = ()=>{}
+  const [values, setvalues] = useState({
+    dateTime: new Date(),
+    description: "",
+    link: "",
+  });
+
+  const { toast } = useToast();
+
+  const [callDetails, setcallDetails] = useState<Call>()
+
+
+  const { user } = useUser();
+  const client = useStreamVideoClient();
+
+  const createMeeting = async () => {
+    if (!user || !client) return;
+    try {
+
+      if(!values.dateTime) {
+        toast({title:"Please select a date and time"});
+         return
+      }
+
+      const id = crypto.randomUUID();
+      const call = client.call("default", id);
+
+      if (!call) throw new Error("Failed to create call");
+
+      const startsAt = values.dateTime.toISOString() || new Date(Date.now()).toISOString();
+      const desciption = values.description || 'Instant meeting';
+
+      await call.getOrCreate({
+        data:{
+          starts_at:startsAt,
+          custom:{
+            desciption
+          }
+        }
+      })
+
+      setcallDetails(call);
+
+      if(!values.description) router.push(`/meeting/${call.id}`)
+        toast({title:'Meeting Created'})
+    } catch (e) {
+      toast({
+        title: "Failed to create meeting",
+      })
+    }
+  };
 
   return (
     <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
-<HomeCard
+      <HomeCard
         img="/icons/add-meeting.svg"
         title="New Meeting"
         description="Start an instant meeting"
-        handleClick={() => setMeetingState('isInstantMeeting')}
+        handleClick={() => setMeetingState("isInstantMeeting")}
       />
       <HomeCard
         img="/icons/join-meeting.svg"
         title="Join Meeting"
         description="via invitation link"
         className="bg-blue-1"
-        handleClick={() => setMeetingState('isJoiningMeeting')}
+        handleClick={() => setMeetingState("isJoiningMeeting")}
       />
       <HomeCard
         img="/icons/schedule.svg"
         title="Schedule Meeting"
         description="Plan your meeting"
         className="bg-purple-1"
-        handleClick={() => setMeetingState('isScheduleMeeting')}
+        handleClick={() => setMeetingState("isScheduleMeeting")}
       />
       <HomeCard
         img="/icons/recordings.svg"
         title="View Recordings"
         description="Meeting Recordings"
         className="bg-yellow-1"
-        handleClick={() => router.push('/recordings')}
+        handleClick={() => router.push("/recordings")}
       />
 
-    <MeetingModal isOpen={meetingState === 'isInstantMeeting'} onClose={()=>setMeetingState(undefined)} 
-    title='Start an instant meeting' className='text-center' buttonText='Start Meeting' handleClick={createMeeting}/>
-
+      <MeetingModal
+        isOpen={meetingState === "isInstantMeeting"}
+        onClose={() => setMeetingState(undefined)}
+        title="Start an instant meeting"
+        className="text-center"
+        buttonText="Start Meeting"
+        handleClick={createMeeting}
+      />
     </section>
-  )
-}
+  );
+};
 
-export default MeetingTypeList
+export default MeetingTypeList;
